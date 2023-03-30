cf202956b9f8f3eb96294f97a682a09d520f2c4c
RafaelKeiti <keitiazuma@gmail.com>
2023-03-30 18:42:34 -0300

    refactor: Alterando valores que foram atribuidos e criando constantes.

--- List-1/src/exercice_14.java
+++ List-1/src/exercice_14.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
 
 public class exercice_14 {
+
+    public static final int LIMIT_NUMBER_KG = 50;
+    public static final int AMOUNT_FINE = 4;
+    public static final int VALUE_INVERTER = -1;
+    public static final int BASE_VALUE = 0;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double weightFish = 0;
-        double amountOfTheFine = 0;
-        double result = 0;
+        double weightFish;
+        double amountOfTheFine;
+        double result;
 
         System.out.println("Digite o peso dos peixes:");
         weightFish = input.nextDouble();
 
-        result = 50 - weightFish;
-        if (result < 0) {
-            amountOfTheFine = (result * 4) * -1;
+        result = LIMIT_NUMBER_KG - weightFish;
+        if (result < BASE_VALUE) {
+            amountOfTheFine = (result * AMOUNT_FINE) * VALUE_INVERTER;
             System.out.println("Você ultrapassou os 50KG e deve pagar uma multa de: R$" + amountOfTheFine);
         } else {
             System.out.println("Você não ultrapassou o peso limite.");
