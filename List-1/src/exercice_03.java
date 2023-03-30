9f4968fbc36af24d195dc79fcacccf5bf9ef8eba
RafaelKeiti <keitiazuma@gmail.com>
2023-03-30 18:15:26 -0300

    refactor: Alterando nomes e valores que foram atribuidos a s váriaveis.

--- List-1/src/exercice_03.java
+++ List-1/src/exercice_03.java
@@ -4,17 +4,17 @@ public class exercice_03 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double number1 = 0;
-        double number2 = 0;
-        double totalSum = 0;
+        double firstNumber;
+        double secondNumber;
+        double totalSum;
 
         System.out.println("Digite um número:");
-        number1 = input.nextDouble();
+        firstNumber = input.nextDouble();
 
         System.out.println("Digite o próximo valor:");
-        number2 = input.nextDouble();
+        secondNumber = input.nextDouble();
 
-        totalSum = number1 + number2;
+        totalSum = firstNumber + secondNumber;
         System.out.println("O total da soma é: " + totalSum);
     }
 }
