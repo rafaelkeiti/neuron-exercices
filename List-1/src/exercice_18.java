31291c3c8b383843c1ffe1a310678ae58f52d24e
RafaelKeiti <keitiazuma@gmail.com>
2023-03-30 18:46:28 -0300

    refactor: Alterando valores atribuidos as váriveis e criando constantes.

--- List-1/src/exercice_18.java
+++ List-1/src/exercice_18.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 
 public class exercice_18 {
+
+    public static final int CONVERSION_NUMBER = 8;
+    public static final int NUMBER_TO_CONVERT_TO_MINUTES = 60;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double download = 0;
-        double speedNet = 0;
-        double time = 0;
+        double download;
+        double speedNet;
+        double time;
 
         System.out.println("Digite o tamanho do arquivo em MB:");
         download = input.nextDouble();
@@ -14,7 +18,7 @@ public class exercice_18 {
         System.out.println("Digite a velocidade da internet:");
         speedNet = input.nextDouble();
 
-        time = (download / (speedNet / 8)) / 60;
+        time = (download / (speedNet / CONVERSION_NUMBER)) / NUMBER_TO_CONVERT_TO_MINUTES;
         System.out.printf("O valor em minutos é: %.2f", time);
     }
 }
