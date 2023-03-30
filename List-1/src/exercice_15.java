89ce3603864318beae07b4b9f30f6235a9515a1d
RafaelKeiti <keitiazuma@gmail.com>
2023-03-30 18:43:35 -0300

    refactor: Alterando forma qe os valores que foram declarados as váriaveis e criando constantes.

--- List-1/src/exercice_15.java
+++ List-1/src/exercice_15.java
@@ -1,11 +1,21 @@
 import java.util.Scanner;
 
 public class exercice_15 {
+
+    public static final double INCOME_TAX_PERCENTAGE_DISCOUNT = 0.11;
+    public static final double INSS_PERCENTAGE_DISCOUNT = 0.08;
+    public static final double SYNDICATE_PERCENTAGE_DISCOUNT = 0.05;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int workedHours = 0;
-        double earningsPerHour = 0, salaryTotal = 0, incomeTax = 0, inss = 0, syndicate = 0, netSalary = 0;
+        int workedHours;
+        double earningsPerHour;
+        double salaryTotal;
+        double incomeTax;
+        double inss;
+        double syndicate;
+        double netSalary;
 
         System.out.println("Quanto você recebe por horas trabalhadas?");
         earningsPerHour = input.nextDouble();
@@ -14,9 +24,9 @@ public class exercice_15 {
         workedHours = input.nextInt();
 
         salaryTotal = earningsPerHour * workedHours;
-        incomeTax = salaryTotal * 0.11;
-        inss = (salaryTotal - incomeTax) * 0.08;
-        syndicate = (salaryTotal - inss) * 0.05;
+        incomeTax = salaryTotal * INCOME_TAX_PERCENTAGE_DISCOUNT;
+        inss = (salaryTotal - incomeTax) * INSS_PERCENTAGE_DISCOUNT;
+        syndicate = (salaryTotal - inss) * SYNDICATE_PERCENTAGE_DISCOUNT;
         netSalary = (salaryTotal - incomeTax - inss - syndicate);
 
         System.out.println("O salário bruto é de: " + salaryTotal +
