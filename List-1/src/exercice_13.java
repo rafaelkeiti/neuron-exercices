7ed26e834cc1a4910db8d4173948e062a5fb4276
RafaelKeiti <keitiazuma@gmail.com>
2023-03-30 18:41:57 -0300

    refactor: Alterando valores que foram atribuidos as váriaveis, criando constantes e otimizando verificação de sexo.

--- List-1/src/exercice_13.java
+++ List-1/src/exercice_13.java
@@ -1,13 +1,20 @@
-import java.util.Objects;
 import java.util.Scanner;
 
 public class exercice_13 {
+
+    public static final double IDEAL_WEIGHT_FORMULA_NUMBER_MEN = 72.7;
+    public static final int NUMBER_FIFTY_EIGHT_FORMULA_IDEAL_WEIGHT = 58;
+    public static final double IDEAL_WEIGHT_FORMULA_NUMBER_WOMEN = 62.1;
+    public static final double NEGATIVE_NUMBER_IDEAL_WEIGHT_FORMULA_WOMEN = 44.7;
+    public static final String WOMAN_SEX = "F";
+    public static final String MALE_SEX = "M";
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         String sex;
-        double height = 0;
-        double idealWeight = 0;
+        double height;
+        double idealWeight;
 
         System.out.println("Qual é sua altura?");
         height = input.nextDouble();
@@ -15,15 +22,14 @@ public class exercice_13 {
         System.out.println("Você é do sexo M / F?");
         sex = input.next();
 
-        if (Objects.equals(sex, "m") || Objects.equals(sex, "M")) {
-            idealWeight = (72.7 * height) - 58;
+        if (sex.equalsIgnoreCase(WOMAN_SEX)) {
+            idealWeight = (IDEAL_WEIGHT_FORMULA_NUMBER_MEN * height) - NUMBER_FIFTY_EIGHT_FORMULA_IDEAL_WEIGHT;
             System.out.println("O seu peso ideal deve ser: " + idealWeight);
-        } else if (Objects.equals(sex, "f") || Objects.equals(sex, "F")) {
-            idealWeight = (62.1 * height) - 44.7;
+        } else if (sex.equalsIgnoreCase(MALE_SEX)) {
+            idealWeight = (IDEAL_WEIGHT_FORMULA_NUMBER_WOMEN * height) - NEGATIVE_NUMBER_IDEAL_WEIGHT_FORMULA_WOMEN;
             System.out.println("O seu peso ideal deve ser: " + idealWeight);
         } else {
             System.out.println("Você digitou uma opção inválida.");
         }
-
     }
 }
