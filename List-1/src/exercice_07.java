d28c3e85fc89b4993a1007d436554660d0e1cae7
RafaelKeiti <keitiazuma@gmail.com>
2023-03-30 18:30:39 -0300

    refactor: Alterando valores que foram atribuidos as váriaveis e criando constante.

--- List-1/src/exercice_07.java
+++ List-1/src/exercice_07.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
 public class exercice_07 {
+
+    public static final int CONSTANT_NUMBER_TWO = 2;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double base = 0;
-        double height = 0;
-        double result = 0;
+        double base;
+        double height;
+        double result;
 
         System.out.println("Digite o valor da base:");
         base = input.nextDouble();
@@ -14,7 +17,7 @@ public class exercice_07 {
         System.out.println("Digite o valor da altura:");
         height = input.nextDouble();
 
-        result = (base * height) * 2;
+        result = (base * height) * CONSTANT_NUMBER_TWO;
         System.out.println("O dobro da área do quadrado é: " + result);
     }
 }
