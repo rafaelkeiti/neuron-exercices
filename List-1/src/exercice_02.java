f396c29e870cb9c4a0526cde22f548a866020a51
RafaelKeiti <keitiazuma@gmail.com>
2023-03-30 18:14:17 -0300

    refactor: Alterando nome e valor que foi atribuido a váriavel.

--- List-1/src/exercice_02.java
+++ List-1/src/exercice_02.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class exercice_02 {
     public static void main(String[] args) {
-        double number = 0;
+        double typedNumber;
 
         Scanner input = new Scanner(System.in);
         System.out.println("Digite um número:");
-        number = input.nextDouble();
-        System.out.println("Você digitou o número: " + number);
+        typedNumber = input.nextDouble();
+        System.out.println("Você digitou o número: " + typedNumber);
     }
 }
