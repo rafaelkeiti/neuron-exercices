0ca3cf9383a5191b1e2282337ce8d1f09e53f227
RafaelKeiti <keitiazuma@gmail.com>
2023-03-30 18:29:50 -0300

    refactor: Alterando nome/valores que foram atribuidos a s váriaveis e criando constantes.

--- List-1/src/exercice_06.java
+++ List-1/src/exercice_06.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
 public class exercice_06 {
+
+    public static final double PI_VALUE = 3.14;
+    public static final int SQUARED = 2;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double ray = 0;
-        double area = 0;
+        double ray;
+        double areaValue;
 
         System.out.println("Digite o raio do circulo para calcular a área.");
         ray = input.nextDouble();
 
-        area = 3.14 * Math.pow(ray, 2);
-        System.out.println("A área é: " + area);
+        areaValue = PI_VALUE * Math.pow(ray, SQUARED);
+        System.out.println("A área é: " + areaValue);
     }
 }
