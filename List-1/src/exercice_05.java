a52759ba24ba9bdbce9bd1a3eaf2561fcc21b25b
RafaelKeiti <keitiazuma@gmail.com>
2023-03-30 18:20:21 -0300

    refactor: Alterando valores atribuidos as váriaveis e criando constantes.

--- List-1/src/exercice_05.java
+++ List-1/src/exercice_05.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
 public class exercice_05 {
+
+    public static final int METER_VALUE_IN_CM = 100;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double meters = 0;
-        double metersConverted = 0;
+        double meters;
+        double metersConverted;
 
         System.out.println("Digite um valor em metros para converter em cm.");
         meters = input.nextDouble();
 
-        metersConverted = meters * 100;
+        metersConverted = meters * METER_VALUE_IN_CM;
         System.out.println(metersConverted + "cm");
     }
 }
