7515422cc54931e745708dfa1a4f44b1ce61020e
RafaelKeiti <keitiazuma@gmail.com>
2023-03-30 18:45:19 -0300

    refactor: Alterando valores que foram atribuidos as váriaveis e criando constantes.

--- List-1/src/exercice_16.java
+++ List-1/src/exercice_16.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
 
 public class exercice_16 {
+
+    public static final int SQUARE_METERS = 3;
+    public static final int LITERS_OF_PAINT = 18;
+    public static final int VALUE_OF_THE_EIGHTEEN_LITER_CAN = 80;
+    public static final int SMALLER_THAN_A_WHOLE_CAN = 1;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double width = 0;
-        double length = 0;
-        double resultMetroSquare = 0;
-        double resultLiterCan = 0;
-        double resultPrice = 0;
+        double width;
+        double length;
+        double resultMetroSquare;
+        double resultLiterCan;
+        double resultPrice;
 
         System.out.println("Digite a largura:");
         width = input.nextDouble();
@@ -16,11 +22,11 @@ public class exercice_16 {
         System.out.println("Digite o comprimento:");
         length = input.nextDouble();
 
-        resultMetroSquare = (width * length) / 3;
-        resultLiterCan = resultMetroSquare / 18;
-        resultPrice = resultLiterCan * 80;
+        resultMetroSquare = (width * length) / SQUARE_METERS;
+        resultLiterCan = resultMetroSquare / LITERS_OF_PAINT;
+        resultPrice = resultLiterCan * VALUE_OF_THE_EIGHTEEN_LITER_CAN;
 
-        if (resultLiterCan < 1) {
+        if (resultLiterCan < SMALLER_THAN_A_WHOLE_CAN) {
             System.out.printf("Infelizmente vendemos a lata fechada com 18 litros. Você precisaria usar somente %.2f litros.", resultLiterCan);
         } else {
             System.out.printf("O total de tinta é de: %.2f litros, precisaremos de %.2f latas um total de R$ %.2f", resultMetroSquare, resultLiterCan, resultPrice);
