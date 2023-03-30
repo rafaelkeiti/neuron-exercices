c972494fd25c3cdf9a38c9efc2adebf5e6e4b090
RafaelKeiti <keitiazuma@gmail.com>
2023-03-30 18:45:51 -0300

    refactor: Alterando valores que foram atribuidos e criando constantes.

--- List-1/src/exercice_17.java
+++ List-1/src/exercice_17.java
@@ -1,20 +1,28 @@
 import java.util.Scanner;
 
 public class exercice_17 {
+
+    public static final int SQUARE_METERS = 6;
+    public static final int EIGHTEEN_LITER_CANS = 18;
+    public static final int VALUE_OF_THE_EIGHTEEN_LITER_CAN = 80;
+    public static final double LITERS_GALLON_THREE_POINT_SIX = 3.6;
+    public static final int VALUE_OF_THE_THREE_POINT_SIX_LITER_CAN = 25;
+    public static final double ADD_TEN_PERCENT = 1.1;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double width = 0;
-        double length = 0;
-        double resultMetroSquare = 0;
-        int resultLiterCan18 = 0;
-        double priceLiterCan18 = 0;
-        int resultLiterCan36 = 0;
-        double priceLiterCan36 = 0;
-        double ink = 0;
-        int cans = 0;
-        int gallons = 0;
-        double priceCansGallons = 0;
+        double width;
+        double length;
+        double resultMetroSquare;
+        int resultLiterCan18;
+        double priceLiterCan18;
+        int resultLiterCan36;
+        double priceLiterCan36;
+        double ink;
+        int cans;
+        int gallons;
+        double priceCansGallons;
 
         System.out.println("Digite a largura");
         width = input.nextDouble();
@@ -22,18 +30,18 @@ public class exercice_17 {
         System.out.println("Digite o comprimento");
         length = input.nextDouble();
 
-        resultMetroSquare = (width * length) / 6;
+        resultMetroSquare = (width * length) / SQUARE_METERS;
 
-        resultLiterCan18 = (int) Math.ceil(resultMetroSquare / 18);
-        priceLiterCan18 = resultLiterCan18 * 80;
+        resultLiterCan18 = (int) Math.ceil(resultMetroSquare / EIGHTEEN_LITER_CANS);
+        priceLiterCan18 = resultLiterCan18 * VALUE_OF_THE_EIGHTEEN_LITER_CAN;
 
-        resultLiterCan36 = (int) Math.ceil(resultMetroSquare / 3.6);
-        priceLiterCan36 = resultLiterCan36 * 25;
+        resultLiterCan36 = (int) Math.ceil(resultMetroSquare / LITERS_GALLON_THREE_POINT_SIX);
+        priceLiterCan36 = resultLiterCan36 * VALUE_OF_THE_THREE_POINT_SIX_LITER_CAN;
 
-        ink = Math.ceil(resultMetroSquare * 1.1);
-        cans = (int) Math.ceil(ink / 18);
-        gallons = (int) Math.ceil((ink % 18) / 3.6);
-        priceCansGallons = (cans * 80) + (gallons * 25);
+        ink = Math.ceil(resultMetroSquare * ADD_TEN_PERCENT);
+        cans = (int) Math.ceil(ink / EIGHTEEN_LITER_CANS);
+        gallons = (int) Math.ceil((ink % EIGHTEEN_LITER_CANS) / LITERS_GALLON_THREE_POINT_SIX);
+        priceCansGallons = (cans * VALUE_OF_THE_EIGHTEEN_LITER_CAN) + (gallons * VALUE_OF_THE_THREE_POINT_SIX_LITER_CAN);
 
         System.out.println("Será necessário " + resultLiterCan18 + " latas, um total de R$" + priceLiterCan18);
         System.out.println(" ");
@@ -41,7 +49,5 @@ public class exercice_17 {
         System.out.println(" ");
         System.out.println("Um total de " + cans + " latas e um total de " + gallons + " galões.\nValor total " +
                 "de R$" + priceCansGallons);
-
-
     }
 }
