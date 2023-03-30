cd5413790658e28d03319bf329577515296c90ff
RafaelKeiti <keitiazuma@gmail.com>
2023-03-30 18:38:43 -0300

    refactor: Alterando valores que foram atribuidos as váriaveis e criando constantes.

--- List-1/src/exercice_09.java
+++ List-1/src/exercice_09.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 
 public class exercice_09 {
+
+    public static final int NUMBER_FIVE_CONST_FORMULA = 5;
+    public static final int NUMBER_THIRTYTWO_CONST_FORMULA = 32;
+    public static final int NUMBER_NINE_CONST_FORMULA = 9;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int FahrenheitTemperature = 0;
-        int result = 0;
+        int FahrenheitTemperature;
+        int result;
 
         System.out.println("Digite uma temperatura em fahrenheit:");
         FahrenheitTemperature = input.nextInt();
 
-        result = 5 * ((FahrenheitTemperature - 32) / 9);
+        result = NUMBER_FIVE_CONST_FORMULA * ((FahrenheitTemperature - NUMBER_THIRTYTWO_CONST_FORMULA) / NUMBER_NINE_CONST_FORMULA);
         System.out.println(result + " Celsius.");
     }
 }
