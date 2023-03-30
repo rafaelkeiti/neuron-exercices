7da0c04295cee94316764b9236e4cff225a2c492
RafaelKeiti <keitiazuma@gmail.com>
2023-03-30 18:40:21 -0300

    refactor: Alterando nomes/valores que foram atribuidos as váriaveis e criando constantes.

--- List-1/src/exercice_11.java
+++ List-1/src/exercice_11.java
@@ -1,28 +1,34 @@
 import java.util.Scanner;
 
 public class exercice_11 {
+
+    public static final int DOUBLE = 2;
+    public static final int HALF = 2;
+    public static final int TRIPLE = 3;
+    public static final int THREETOTHE_REAL_NUMBER = 3;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int number1 = 0;
-        int number2 = 0;
-        double realNumber = 0;
-        int result = 0;
-        double result2 = 0;
-        double result3 = 0;
+        int firstNumber;
+        int secondNumber;
+        double realNumber;
+        int firstResult;
+        double secondResult;
+        double thirdResult;
 
         System.out.println("Digite um número inteiro:");
-        number1 = input.nextInt();
+        firstNumber = input.nextInt();
 
         System.out.println("Digite outro número inteiro:");
-        number2 = input.nextInt();
+        secondNumber = input.nextInt();
 
         System.out.println("Digite um número real:");
         realNumber = input.nextDouble();
 
-        result = (number1 * 2) * (number2 / 2);
-        result2 = (number1 * 3) + realNumber;
-        result3 = Math.pow(realNumber, 3);
-        System.out.println("A) " + result + "\nB) " + result2 + "\nC) " + result3);
+        firstResult = (firstNumber * DOUBLE) * (secondNumber / HALF);
+        secondResult = (firstNumber * TRIPLE) + realNumber;
+        thirdResult = Math.pow(realNumber, THREETOTHE_REAL_NUMBER);
+        System.out.println("A) " + firstResult + "\nB) " + secondResult + "\nC) " + thirdResult);
     }
 }
