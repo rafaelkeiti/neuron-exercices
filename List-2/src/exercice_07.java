3140f72aa6a5d8bd285139a4e0768b3c4512afa3
RafaelKeiti <keitiazuma@gmail.com>
2023-03-30 21:11:59 -0300

    refactor: Alterando nomes/valores que foram atribuidos as váriaveis.

---
+++ List-2/src/exercice_07.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class exercice_07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int firstNumber;
+        int secondNumber;
+        int thirdNumber;
+
+        System.out.println("Digite um número:");
+        firstNumber = input.nextInt();
+
+        System.out.println("Digite outro número:");
+        secondNumber = input.nextInt();
+
+        System.out.println("Digite mais um número:");
+        thirdNumber = input.nextInt();
+
+        if (firstNumber > secondNumber && firstNumber > thirdNumber) {
+            System.out.println("O maior digitado foi o " + firstNumber);
+        } else if (secondNumber > firstNumber && secondNumber > thirdNumber) {
+            System.out.println("O maior digitado foi o " + secondNumber);
+        } else if (thirdNumber > firstNumber && thirdNumber > secondNumber) {
+            System.out.println("O maior ditado foi o " + thirdNumber);
+        }
+        if (firstNumber < secondNumber && firstNumber < thirdNumber) {
+            System.out.println("O menor digitado foi o " + firstNumber);
+        } else if (secondNumber < firstNumber && secondNumber < thirdNumber) {
+            System.out.println("O menor digitado foi o " + secondNumber);
+        } else if (thirdNumber < firstNumber && thirdNumber < secondNumber) {
+            System.out.println("O menor digitado foi o " + thirdNumber);
+        }
+    }
+}
