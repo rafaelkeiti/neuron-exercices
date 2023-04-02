6e873cbf059d31cb203b5d4ecacd16cea28cbf94
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 20:35:43 -0300

    refactor: Deixando as constantes privadas e alterando forma de inicializar as variáveis.

--- List-1/src/exercice_12.java
+++ List-1/src/exercice_12.java
@@ -2,19 +2,16 @@ import java.util.Scanner;
 
 public class exercice_12 {
 
-    public static final double IDEAL_WEIGHT_FORMULA_NUMBER = 72.7;
-    public static final int NUMBER_FIFTYEIGHT_FORMULA_IDEAL_WEIGHT = 58;
+    private static final double IDEAL_WEIGHT_FORMULA_NUMBER = 72.7;
+    private static final int NUMBER_FIFTYEIGHT_FORMULA_IDEAL_WEIGHT = 58;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double height;
-        double idealWeight;
-
         System.out.println("Digite sua altura:");
-        height = input.nextDouble();
+        double height = input.nextDouble();
 
-        idealWeight = (IDEAL_WEIGHT_FORMULA_NUMBER * height) - NUMBER_FIFTYEIGHT_FORMULA_IDEAL_WEIGHT;
+        double idealWeight = (IDEAL_WEIGHT_FORMULA_NUMBER * height) - NUMBER_FIFTYEIGHT_FORMULA_IDEAL_WEIGHT;
         System.out.println("Seu peso ideal é: " + idealWeight);
     }
 }
