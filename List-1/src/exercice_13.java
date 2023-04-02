928a5afb3ae3bf05d3db6c68b4ee754ce44d7886
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 20:37:17 -0300

    refactor: Deixando as constantes privadas e alterando a forma de inicializar as variáveis.

--- List-1/src/exercice_13.java
+++ List-1/src/exercice_13.java
@@ -2,31 +2,27 @@ import java.util.Scanner;
 
 public class exercice_13 {
 
-    public static final double IDEAL_WEIGHT_FORMULA_NUMBER_MEN = 72.7;
-    public static final int NUMBER_FIFTY_EIGHT_FORMULA_IDEAL_WEIGHT = 58;
-    public static final double IDEAL_WEIGHT_FORMULA_NUMBER_WOMEN = 62.1;
-    public static final double NEGATIVE_NUMBER_IDEAL_WEIGHT_FORMULA_WOMEN = 44.7;
-    public static final String WOMAN_SEX = "F";
-    public static final String MALE_SEX = "M";
+    private static final double IDEAL_WEIGHT_FORMULA_NUMBER_MEN = 72.7;
+    private static final int NUMBER_FIFTY_EIGHT_FORMULA_IDEAL_WEIGHT = 58;
+    private static final double IDEAL_WEIGHT_FORMULA_NUMBER_WOMEN = 62.1;
+    private static final double NEGATIVE_NUMBER_IDEAL_WEIGHT_FORMULA_WOMEN = 44.7;
+    private static final String WOMAN_SEX = "F";
+    private static final String MALE_SEX = "M";
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String sex;
-        double height;
-        double idealWeight;
-
         System.out.println("Qual é sua altura?");
-        height = input.nextDouble();
+        double height = input.nextDouble();
 
         System.out.println("Você é do sexo M / F?");
-        sex = input.next();
+        String sex = input.next();
 
         if (sex.equalsIgnoreCase(WOMAN_SEX)) {
-            idealWeight = (IDEAL_WEIGHT_FORMULA_NUMBER_MEN * height) - NUMBER_FIFTY_EIGHT_FORMULA_IDEAL_WEIGHT;
+            double idealWeight = (IDEAL_WEIGHT_FORMULA_NUMBER_MEN * height) - NUMBER_FIFTY_EIGHT_FORMULA_IDEAL_WEIGHT;
             System.out.println("O seu peso ideal deve ser: " + idealWeight);
         } else if (sex.equalsIgnoreCase(MALE_SEX)) {
-            idealWeight = (IDEAL_WEIGHT_FORMULA_NUMBER_WOMEN * height) - NEGATIVE_NUMBER_IDEAL_WEIGHT_FORMULA_WOMEN;
+            double idealWeight = (IDEAL_WEIGHT_FORMULA_NUMBER_WOMEN * height) - NEGATIVE_NUMBER_IDEAL_WEIGHT_FORMULA_WOMEN;
             System.out.println("O seu peso ideal deve ser: " + idealWeight);
         } else {
             System.out.println("Você digitou uma opção inválida.");
