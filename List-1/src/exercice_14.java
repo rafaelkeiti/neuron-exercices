85b540ba74e1ac963decb2f223caa1aac827ba1c
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 20:38:13 -0300

    refactor: Deixando as constantes privadas e alterando forma de inicializar as variáveis.

--- List-1/src/exercice_14.java
+++ List-1/src/exercice_14.java
@@ -2,24 +2,20 @@ import java.util.Scanner;
 
 public class exercice_14 {
 
-    public static final int LIMIT_NUMBER_KG = 50;
-    public static final int AMOUNT_FINE = 4;
-    public static final int VALUE_INVERTER = -1;
-    public static final int BASE_VALUE = 0;
+    private static final int LIMIT_NUMBER_KG = 50;
+    private static final int AMOUNT_FINE = 4;
+    private static final int VALUE_INVERTER = -1;
+    private static final int BASE_VALUE = 0;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double weightFish;
-        double amountOfTheFine;
-        double result;
-
         System.out.println("Digite o peso dos peixes:");
-        weightFish = input.nextDouble();
+        double weightFish = input.nextDouble();
 
-        result = LIMIT_NUMBER_KG - weightFish;
+        double result = LIMIT_NUMBER_KG - weightFish;
         if (result < BASE_VALUE) {
-            amountOfTheFine = (result * AMOUNT_FINE) * VALUE_INVERTER;
+            double amountOfTheFine = (result * AMOUNT_FINE) * VALUE_INVERTER;
             System.out.println("Você ultrapassou os 50KG e deve pagar uma multa de: R$" + amountOfTheFine);
         } else {
             System.out.println("Você não ultrapassou o peso limite.");
