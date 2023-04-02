cf744c1ad9feb6f57b1fa90a8108cd7a912aaabd
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 20:09:18 -0300

    refactor: Alterando forma de inicializar a váriavel.

--- List-1/src/exercice_02.java
+++ List-1/src/exercice_02.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class exercice_02 {
     public static void main(String[] args) {
-        double typedNumber;
-
         Scanner input = new Scanner(System.in);
+
         System.out.println("Digite um número:");
-        typedNumber = input.nextDouble();
+        double typedNumber = input.nextDouble();
+
         System.out.println("Você digitou o número: " + typedNumber);
     }
 }
