b96b43fd5c2096eb40ae4b2bb27c11f79e9125fb
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 20:32:34 -0300

    refactor: Deixando as constantes privadas e alterando forma de inicializar as variáveis.

--- List-1/src/exercice_09.java
+++ List-1/src/exercice_09.java
@@ -2,20 +2,17 @@ import java.util.Scanner;
 
 public class exercice_09 {
 
-    public static final int NUMBER_FIVE_CONST_FORMULA = 5;
-    public static final int NUMBER_THIRTYTWO_CONST_FORMULA = 32;
-    public static final int NUMBER_NINE_CONST_FORMULA = 9;
+    private static final int NUMBER_FIVE_CONST_FORMULA = 5;
+    private static final int NUMBER_THIRTYTWO_CONST_FORMULA = 32;
+    private static final int NUMBER_NINE_CONST_FORMULA = 9;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int FahrenheitTemperature;
-        int result;
-
         System.out.println("Digite uma temperatura em fahrenheit:");
-        FahrenheitTemperature = input.nextInt();
+        int fahrenheitTemperature = input.nextInt();
 
-        result = NUMBER_FIVE_CONST_FORMULA * ((FahrenheitTemperature - NUMBER_THIRTYTWO_CONST_FORMULA) / NUMBER_NINE_CONST_FORMULA);
+        int result = NUMBER_FIVE_CONST_FORMULA * ((fahrenheitTemperature - NUMBER_THIRTYTWO_CONST_FORMULA) / NUMBER_NINE_CONST_FORMULA);
         System.out.println(result + " Celsius.");
     }
 }
