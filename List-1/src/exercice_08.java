a7dabfdcfd4a8c0495a3c37f6d94caec33a1e604
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 20:51:41 -0300

    refactor: Alterando forma de inicializar as variáveis.

--- List-1/src/exercice_08.java
+++ List-1/src/exercice_08.java
@@ -4,17 +4,13 @@ public class exercice_08 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double moneyPerHour;
-        int hoursWork;
-        double totalSalary;
-
         System.out.println("Digite o valor que você ganha por hora:");
-        moneyPerHour = input.nextDouble();
+        double moneyPerHour = input.nextDouble();
 
         System.out.println("Digite a quantidade de horas que você trabalha:");
-        hoursWork = input.nextInt();
+        int hoursWork = input.nextInt();
 
-        totalSalary = moneyPerHour * hoursWork;
+        double totalSalary = moneyPerHour * hoursWork;
         System.out.println("Você ganha por mês: R$ " + totalSalary);
     }
 }
