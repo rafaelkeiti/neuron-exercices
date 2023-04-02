966e1aab86f1c68ad6d636c6a903b215afe1b17c
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 20:10:42 -0300

    refactor: Alterando forma de inicializar as váriaveis.

--- List-1/src/exercice_03.java
+++ List-1/src/exercice_03.java
@@ -4,17 +4,13 @@ public class exercice_03 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double firstNumber;
-        double secondNumber;
-        double totalSum;
-
         System.out.println("Digite um número:");
-        firstNumber = input.nextDouble();
+        double firstNumber = input.nextDouble();
 
         System.out.println("Digite o próximo valor:");
-        secondNumber = input.nextDouble();
+        double secondNumber = input.nextDouble();
 
-        totalSum = firstNumber + secondNumber;
+        double totalSum = firstNumber + secondNumber;
         System.out.println("O total da soma é: " + totalSum);
     }
 }
