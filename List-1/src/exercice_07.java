32b6b87883ba122f4f78646852f9633f7e91e5d1
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 20:23:43 -0300

    refactor: Deixando a constante privada e alterando forma de inicializar as variáveis.

--- List-1/src/exercice_07.java
+++ List-1/src/exercice_07.java
@@ -2,22 +2,18 @@ import java.util.Scanner;
 
 public class exercice_07 {
 
-    public static final int CONSTANT_NUMBER_TWO = 2;
+    private static final int CONSTANT_NUMBER_TWO = 2;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double base;
-        double height;
-        double result;
-
         System.out.println("Digite o valor da base:");
-        base = input.nextDouble();
+        double base = input.nextDouble();
 
         System.out.println("Digite o valor da altura:");
-        height = input.nextDouble();
+        double height = input.nextDouble();
 
-        result = (base * height) * CONSTANT_NUMBER_TWO;
+        double result = (base * height) * CONSTANT_NUMBER_TWO;
         System.out.println("O dobro da área do quadrado é: " + result);
     }
 }
