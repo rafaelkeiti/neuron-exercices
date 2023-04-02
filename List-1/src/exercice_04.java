dd8dde0a09aeaa2d4b2b0f626025c93bbc8b999a
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 20:16:22 -0300

    refactor: Alterando forma de inicializar as váriaveis.

--- List-1/src/exercice_04.java
+++ List-1/src/exercice_04.java
@@ -4,19 +4,17 @@ public class exercice_04 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int amountOfNotes;
         double proofNote = 0;
-        double totalAverage;
 
         System.out.println("(Digite quantas notas você quer obter a média.)");
-        amountOfNotes = input.nextInt();
+        int amountOfNotes = input.nextInt();
 
         for (int i = 0; i < amountOfNotes; i++) {
             System.out.println("Nota: ");
             proofNote += input.nextDouble();
         }
 
-        totalAverage = proofNote / amountOfNotes;
+        double totalAverage = proofNote / amountOfNotes;
         System.out.println("Sua média foi de: " + totalAverage);
     }
 }
