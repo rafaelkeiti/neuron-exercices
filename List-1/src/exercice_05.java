4f647220c5283d65ec9edb15c5911c0b094f53b3
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 20:19:41 -0300

    refactor: Deixando as constantes privadas e alterando forma de inicializar as vÃariáveis.

--- List-1/src/exercice_05.java
+++ List-1/src/exercice_05.java
@@ -2,18 +2,15 @@ import java.util.Scanner;
 
 public class exercice_05 {
 
-    public static final int METER_VALUE_IN_CM = 100;
+    private static final int METER_VALUE_IN_CM = 100;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double meters;
-        double metersConverted;
-
         System.out.println("Digite um valor em metros para converter em cm.");
-        meters = input.nextDouble();
+        double meters = input.nextDouble();
 
-        metersConverted = meters * METER_VALUE_IN_CM;
+        double metersConverted = meters * METER_VALUE_IN_CM;
         System.out.println(metersConverted + "cm");
     }
 }
