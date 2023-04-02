d1751f2f9988360ca1e1b642577f705ac7b604bf
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 20:47:43 -0300

    refactor: Deixando as constantes privadas e alterando forma de inicializar as variáveis.

--- List-1/src/exercice_18.java
+++ List-1/src/exercice_18.java
@@ -2,23 +2,19 @@ import java.util.Scanner;
 
 public class exercice_18 {
 
-    public static final int CONVERSION_NUMBER = 8;
-    public static final int NUMBER_TO_CONVERT_TO_MINUTES = 60;
+    private static final int CONVERSION_NUMBER = 8;
+    private static final int NUMBER_TO_CONVERT_TO_MINUTES = 60;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double download;
-        double speedNet;
-        double time;
-
         System.out.println("Digite o tamanho do arquivo em MB:");
-        download = input.nextDouble();
+        double download = input.nextDouble();
 
         System.out.println("Digite a velocidade da internet:");
-        speedNet = input.nextDouble();
+        double speedNet = input.nextDouble();
 
-        time = (download / (speedNet / CONVERSION_NUMBER)) / NUMBER_TO_CONVERT_TO_MINUTES;
+        double time = (download / (speedNet / CONVERSION_NUMBER)) / NUMBER_TO_CONVERT_TO_MINUTES;
         System.out.printf("O valor em minutos é: %.2f", time);
     }
 }
