202cb68f5fad3f9d216abd8a92b6fd53dbc6ea8d
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 20:41:47 -0300

    refactor: Deixando as constantes privadas e alterando forma de iniciar as variáveis.

--- List-1/src/exercice_16.java
+++ List-1/src/exercice_16.java
@@ -2,29 +2,23 @@ import java.util.Scanner;
 
 public class exercice_16 {
 
-    public static final int SQUARE_METERS = 3;
-    public static final int LITERS_OF_PAINT = 18;
-    public static final int VALUE_OF_THE_EIGHTEEN_LITER_CAN = 80;
-    public static final int SMALLER_THAN_A_WHOLE_CAN = 1;
+    private static final int SQUARE_METERS = 3;
+    private static final int LITERS_OF_PAINT = 18;
+    private static final int VALUE_OF_THE_EIGHTEEN_LITER_CAN = 80;
+    private static final int SMALLER_THAN_A_WHOLE_CAN = 1;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double width;
-        double length;
-        double resultMetroSquare;
-        double resultLiterCan;
-        double resultPrice;
-
         System.out.println("Digite a largura:");
-        width = input.nextDouble();
+        double width = input.nextDouble();
 
         System.out.println("Digite o comprimento:");
-        length = input.nextDouble();
+        double length = input.nextDouble();
 
-        resultMetroSquare = (width * length) / SQUARE_METERS;
-        resultLiterCan = resultMetroSquare / LITERS_OF_PAINT;
-        resultPrice = resultLiterCan * VALUE_OF_THE_EIGHTEEN_LITER_CAN;
+        double resultMetroSquare = (width * length) / SQUARE_METERS;
+        double resultLiterCan = resultMetroSquare / LITERS_OF_PAINT;
+        double resultPrice = resultLiterCan * VALUE_OF_THE_EIGHTEEN_LITER_CAN;
 
         if (resultLiterCan < SMALLER_THAN_A_WHOLE_CAN) {
             System.out.printf("Infelizmente vendemos a lata fechada com 18 litros. Você precisaria usar somente %.2f litros.", resultLiterCan);
