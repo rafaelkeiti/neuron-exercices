da5c859bfe3f6d117166bf680fa61c015a545c1b
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 20:40:46 -0300

    refactor: Deixando as constantes privadas e alterando forma de iniciar as variáveis.

--- List-1/src/exercice_15.java
+++ List-1/src/exercice_15.java
@@ -2,32 +2,24 @@ import java.util.Scanner;
 
 public class exercice_15 {
 
-    public static final double INCOME_TAX_PERCENTAGE_DISCOUNT = 0.11;
-    public static final double INSS_PERCENTAGE_DISCOUNT = 0.08;
-    public static final double SYNDICATE_PERCENTAGE_DISCOUNT = 0.05;
+    private static final double INCOME_TAX_PERCENTAGE_DISCOUNT = 0.11;
+    private static final double INSS_PERCENTAGE_DISCOUNT = 0.08;
+    private static final double SYNDICATE_PERCENTAGE_DISCOUNT = 0.05;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int workedHours;
-        double earningsPerHour;
-        double salaryTotal;
-        double incomeTax;
-        double inss;
-        double syndicate;
-        double netSalary;
-
         System.out.println("Quanto você recebe por horas trabalhadas?");
-        earningsPerHour = input.nextDouble();
+        double earningsPerHour = input.nextDouble();
 
         System.out.println("Quantas horas você trabalha por mês?");
-        workedHours = input.nextInt();
+        int workedHours = input.nextInt();
 
-        salaryTotal = earningsPerHour * workedHours;
-        incomeTax = salaryTotal * INCOME_TAX_PERCENTAGE_DISCOUNT;
-        inss = (salaryTotal - incomeTax) * INSS_PERCENTAGE_DISCOUNT;
-        syndicate = (salaryTotal - inss) * SYNDICATE_PERCENTAGE_DISCOUNT;
-        netSalary = (salaryTotal - incomeTax - inss - syndicate);
+        double salaryTotal = earningsPerHour * workedHours;
+        double incomeTax = salaryTotal * INCOME_TAX_PERCENTAGE_DISCOUNT;
+        double inss = (salaryTotal - incomeTax) * INSS_PERCENTAGE_DISCOUNT;
+        double syndicate = (salaryTotal - inss) * SYNDICATE_PERCENTAGE_DISCOUNT;
+        double netSalary = (salaryTotal - incomeTax - inss - syndicate);
 
         System.out.println("O salário bruto é de: " + salaryTotal +
                 "\nO IR foi de: " + incomeTax + "\nINSS: " + inss +
