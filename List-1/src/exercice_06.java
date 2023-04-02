200cbc22b82c3b2ab8c7cf6347fd1afed06b6bd0
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 20:22:21 -0300

    refactor: Deixando as constantes privadas e alterando forma de inicializar as variáveis.

--- List-1/src/exercice_06.java
+++ List-1/src/exercice_06.java
@@ -2,19 +2,16 @@ import java.util.Scanner;
 
 public class exercice_06 {
 
-    public static final double PI_VALUE = 3.14;
-    public static final int SQUARED = 2;
+    private static final double PI_VALUE = 3.14;
+    private static final int SQUARED = 2;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double ray;
-        double areaValue;
-
         System.out.println("Digite o raio do circulo para calcular a área.");
-        ray = input.nextDouble();
+        double ray = input.nextDouble();
 
-        areaValue = PI_VALUE * Math.pow(ray, SQUARED);
+        double areaValue = PI_VALUE * Math.pow(ray, SQUARED);
         System.out.println("A área é: " + areaValue);
     }
 }
