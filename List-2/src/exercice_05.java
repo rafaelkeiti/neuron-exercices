a3145c5b9d5c0fad1b4b009c906213d669a66493
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 22:10:58 -0300

    refactor: Deixando as constantes privadas e alterando forma de inicializar as variáveis.

--- List-2/src/exercice_05.java
+++ List-2/src/exercice_05.java
@@ -2,23 +2,19 @@ import java.util.Scanner;
 
 public class exercice_05 {
 
-    public static final int AVERAGE_PASSED = 7;
-    public static final int FULL_MARK = 10;
+    private static final int AVERAGE_PASSED = 7;
+    private static final int FULL_MARK = 10;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double firstTestGrade;
-        double secondTestGrade;
-        double average;
-
         System.out.println("Digite a primeira nota:");
-        firstTestGrade = input.nextDouble();
+        double firstTestGrade = input.nextDouble();
 
         System.out.println("Digite a segunda nota:");
-        secondTestGrade = input.nextDouble();
+        double secondTestGrade = input.nextDouble();
 
-        average = (firstTestGrade + secondTestGrade) / 2;
+        double average = (firstTestGrade + secondTestGrade) / 2;
         if (average >= AVERAGE_PASSED && average < FULL_MARK) {
             System.out.println("Aprovado.");
         } else if (average < AVERAGE_PASSED) {
