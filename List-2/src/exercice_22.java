da61c1aada2d595bfc5b704e1af86fedcd91a229
RafaelKeiti <keitiazuma@gmail.com>
2023-04-03 00:13:18 -0300

    refactor: Deixando as constantes privadas e alterando forma de inicializar a variável.

--- List-2/src/exercice_22.java
+++ List-2/src/exercice_22.java
@@ -2,16 +2,14 @@ import java.util.Scanner;
 
 public class exercice_22 {
 
-    public static final int TWO = 2;
-    public static final int ZERO = 0;
+    private static final int TWO = 2;
+    private static final int ZERO = 0;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int firstNumber;
-
         System.out.println("Digite um número para saber se ele é par ou ímpar:");
-        firstNumber = input.nextInt();
+        int firstNumber = input.nextInt();
 
         if (firstNumber % TWO == ZERO) {
             System.out.println("O número " + firstNumber + " é par.");
