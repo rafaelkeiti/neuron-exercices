84d5fd7a8d06273219cc6a50a461eaf67dc55d7d
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 23:47:16 -0300

    refactor: Alterando forma de inicializar as variáveis.

--- List-2/src/exercice_17.java
+++ List-2/src/exercice_17.java
@@ -2,19 +2,16 @@ import java.util.Scanner;
 
 public class exercice_17 {
 
-    public static final int TWO = 2;
-    public static final int ZERO = 0;
+    private static final int TWO = 2;
+    private static final int ZERO = 0;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int year;
-        int leapYear;
-
         System.out.println("Digite um número:");
-        year = input.nextInt();
+        int year = input.nextInt();
 
-        leapYear = year % TWO;
+        int leapYear = year % TWO;
 
         if (leapYear == ZERO) {
             System.out.println(year + " é um year bissexto.");
