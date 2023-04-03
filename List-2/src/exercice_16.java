96e5834943dae4a8596354c11e75e5dcd68a52e2
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 23:46:20 -0300

    refactor: Alterando forma de inicializar as variáveis.

--- List-2/src/exercice_16.java
+++ List-2/src/exercice_16.java
@@ -2,23 +2,16 @@ import java.util.Scanner;
 
 public class exercice_16 {
 
-    public static final int ZERO = 0;
-    public static final int SQUARED = 2;
-    public static final int FOUR_DELTA = 4;
-    public static final int TWO = 2;
+    private static final int ZERO = 0;
+    private static final int SQUARED = 2;
+    private static final int FOUR_DELTA = 4;
+    private static final int TWO = 2;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double a;
-        double b;
-        double c;
-        double delta;
-        double x1;
-        double x2;
-
         System.out.println("Digite o valor de a:");
-        a = input.nextDouble();
+        double a = input.nextDouble();
 
         if (a == ZERO) {
             System.out.println("A equação não é de segundo grau!");
@@ -26,14 +19,14 @@ public class exercice_16 {
         }
 
         System.out.println("Digite o valor de b:");
-        b = input.nextDouble();
+        double b = input.nextDouble();
 
         System.out.println("Digite o valor de c:");
-        c = input.nextDouble();
+        double c = input.nextDouble();
 
-        delta = Math.pow(b, SQUARED) - FOUR_DELTA * a * c;
-        x1 = (-b + Math.sqrt(delta)) / (TWO * a);
-        x2 = (-b - Math.sqrt(delta)) / (TWO * a);
+        double delta = Math.pow(b, SQUARED) - FOUR_DELTA * a * c;
+        double x1 = (-b + Math.sqrt(delta)) / (TWO * a);
+        double x2 = (-b - Math.sqrt(delta)) / (TWO * a);
 
         if (delta < ZERO) {
             System.out.println("Não possui raízes reais.");
