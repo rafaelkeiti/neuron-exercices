7affa402d5f4bdbf7705756b4d6465e017b59d84
RafaelKeiti <keitiazuma@gmail.com>
2023-04-03 00:05:32 -0300

    refactor: Deixando as constantes privadas e alterando forma de inicializar as variáveis.

--- List-2/src/exercice_21.java
+++ List-2/src/exercice_21.java
@@ -2,42 +2,36 @@ import java.util.Scanner;
 
 public class exercice_21 {
 
-    public static final int MINIMUM_WITHDRAWAL = 10;
-    public static final int MAXIMUM_WITHDRAWAL = 600;
-    public static final int NOTES_OF_ONE_HUNDREDS = 100;
-    public static final int FIFTY_NOTES = 50;
-    public static final int TEN_NOTES = 10;
-    public static final int FIVE_NOTES = 5;
-    public static final int ONE_REAL = 1;
+    private static final int MINIMUM_WITHDRAWAL = 10;
+    private static final int MAXIMUM_WITHDRAWAL = 600;
+    private static final int NOTES_OF_ONE_HUNDREDS = 100;
+    private static final int FIFTY_NOTES = 50;
+    private static final int TEN_NOTES = 10;
+    private static final int FIVE_NOTES = 5;
+    private static final int ONE_REAL = 1;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int valueWithdraw;
-        int notesOfOneHundreds;
-        int fiftyNotes;
-        int tenNotes;
-        int fivesNotes;
-        int oneReal;
         System.out.println("Digite quanto deseja sacar:");
-        valueWithdraw = input.nextInt();
+        int valueWithdraw = input.nextInt();
 
         if (valueWithdraw < MINIMUM_WITHDRAWAL || valueWithdraw > MAXIMUM_WITHDRAWAL) {
             System.out.println("Digite valores de 10 até 600.");
         } else {
-            notesOfOneHundreds = valueWithdraw / NOTES_OF_ONE_HUNDREDS;
+            int notesOfOneHundreds = valueWithdraw / NOTES_OF_ONE_HUNDREDS;
             valueWithdraw %= NOTES_OF_ONE_HUNDREDS;
 
-            fiftyNotes = valueWithdraw / FIFTY_NOTES;
+            int fiftyNotes = valueWithdraw / FIFTY_NOTES;
             valueWithdraw %= FIFTY_NOTES;
 
-            tenNotes = valueWithdraw / TEN_NOTES;
+            int tenNotes = valueWithdraw / TEN_NOTES;
             valueWithdraw %= TEN_NOTES;
 
-            fivesNotes = valueWithdraw / FIVE_NOTES;
+            int fivesNotes = valueWithdraw / FIVE_NOTES;
             valueWithdraw %= FIVE_NOTES;
 
-            oneReal = valueWithdraw / ONE_REAL;
+            int oneReal = valueWithdraw / ONE_REAL;
             System.out.println("Notas de 100 reais: " + notesOfOneHundreds);
             System.out.println("Notas de 50 reais: " + fiftyNotes);
             System.out.println("Notas de 10 reais: " + tenNotes);
