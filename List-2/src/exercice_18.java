4e37ed6a1084b8d528388bbb608905b7050b8318
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 23:50:43 -0300

    refactor: Deixando as constantes privadas e alterando forma de inicializar as variáveis.

--- List-2/src/exercice_18.java
+++ List-2/src/exercice_18.java
@@ -2,40 +2,34 @@ import java.util.Scanner;
 
 public class exercice_18 {
 
-    public static final int ZERO = 0;
-    public static final int TWO = 2;
-    public static final int THREE = 3;
-    public static final int FIVE = 5;
-    public static final int SIX = 6;
-    public static final int TEN = 10;
-    public static final int MONTH_ONE = 1;
-    public static final int MONTH_TWELVE = 12;
-    public static final int YEAR_ONE = 1;
-    public static final int MONTH_FOUR = 4;
-    public static final int MONTH_SIX = 6;
-    public static final int MONTH_NINE = 9;
-    public static final int MONTH_ELEVEN = 11;
-    public static final int TOTAL_DAYS = 30;
-    public static final int MONTH_TWO = 2;
-    public static final int FOUR = 4;
-    public static final int TOTAL_DAYS_TWENTY_NINE = 29;
-    public static final int TOTAL_DAYS_TWENTY_EIGHT = 28;
-    public static final int DAY_ONE = 1;
-    public static final int ONE_HUNDRED = 100;
-    public static final int FOUR_HUNDRED = 400;
-    public static final int DAYS_OF_THE_MONTH = 31;
+    private static final int ZERO = 0;
+    private static final int TWO = 2;
+    private static final int THREE = 3;
+    private static final int FIVE = 5;
+    private static final int SIX = 6;
+    private static final int TEN = 10;
+    private static final int MONTH_ONE = 1;
+    private static final int MONTH_TWELVE = 12;
+    private static final int YEAR_ONE = 1;
+    private static final int MONTH_FOUR = 4;
+    private static final int MONTH_SIX = 6;
+    private static final int MONTH_NINE = 9;
+    private static final int MONTH_ELEVEN = 11;
+    private static final int TOTAL_DAYS = 30;
+    private static final int MONTH_TWO = 2;
+    private static final int FOUR = 4;
+    private static final int TOTAL_DAYS_TWENTY_NINE = 29;
+    private static final int TOTAL_DAYS_TWENTY_EIGHT = 28;
+    private static final int DAY_ONE = 1;
+    private static final int ONE_HUNDRED = 100;
+    private static final int FOUR_HUNDRED = 400;
+    private static final int DAYS_OF_THE_MONTH = 31;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String date;
-        int day;
-        int month;
-        int year;
-        int daysOfMonths;
-
         System.out.println("Digite uma date:");
-        date = input.nextLine();
+        String date = input.nextLine();
 
         if (date.matches("\\d{2}/\\d{2}/\\d{4}")) {
             System.out.println("Formato válido.");
@@ -44,11 +38,11 @@ public class exercice_18 {
             return;
         }
 
-        day = Integer.parseInt(date.substring(ZERO, TWO));
-        month = Integer.parseInt(date.substring(THREE, FIVE));
-        year = Integer.parseInt(date.substring(SIX, TEN));
+        int day = Integer.parseInt(date.substring(ZERO, TWO));
+        int month = Integer.parseInt(date.substring(THREE, FIVE));
+        int year = Integer.parseInt(date.substring(SIX, TEN));
 
-        daysOfMonths = DAYS_OF_THE_MONTH;
+        int daysOfMonths = DAYS_OF_THE_MONTH;
 
         if (month < MONTH_ONE || month > MONTH_TWELVE) {
             System.out.println("Mês inválido.");
