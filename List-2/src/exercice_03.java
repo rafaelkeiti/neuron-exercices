27eba2dcd9427e8e1cef3683dbbb7acf2a889d23
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 21:41:20 -0300

    refactor: Deixando as constantes privadas e alterando forma de inicializar a variável.

--- List-2/src/exercice_03.java
+++ List-2/src/exercice_03.java
@@ -2,16 +2,14 @@ import java.util.Scanner;
 
 public class exercice_03 {
 
-    public static final String WOMAN_SEX = "F";
-    public static final String MALE_SEX = "M";
+    private static final String WOMAN_SEX = "F";
+    private static final String MALE_SEX = "M";
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String sex;
-
         System.out.println("Digite (F) para feminino e (M) para masculino:");
-        sex = input.nextLine();
+        String sex = input.nextLine();
 
         if (sex.equalsIgnoreCase(WOMAN_SEX)) {
             System.out.println("Sexo Feminino.");
