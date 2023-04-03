550342e31184c1b3891e235358028713bb9939d1
RafaelKeiti <keitiazuma@gmail.com>
2023-04-02 23:41:15 -0300

    refactor: Deixando as constantes privadas e alterando forma de inicializar as variáveis.

--- List-2/src/exercice_12.java
+++ List-2/src/exercice_12.java
@@ -2,37 +2,30 @@ import java.util.Scanner;
 
 public class exercice_12 {
 
-    public static final double THREE_PERCENT_DISCOUNT = 0.03;
-    public static final double ELEVEN_PERCENT_FGTS = 0.11;
-    public static final int ZERO = 0;
-    public static final int EXEMPTION_FROM_DISCOUNTS = 900;
-    public static final int SALARY_WITH_FIVE_PERCENT_DISCOUNT = 1500;
-    public static final int SALARY_WITH_TEN_PERCENT_DISCOUNT = 2500;
-    public static final double FIVE_PERCENT_DISCOUNT = 0.05;
-    public static final double TEN_PERCENT_DISCOUNT = 0.1;
-    public static final double TWENTY_PERCENT_DISCOUNT = 0.2;
+    private static final double THREE_PERCENT_DISCOUNT = 0.03;
+    private static final double ELEVEN_PERCENT_FGTS = 0.11;
+    private static final int ZERO = 0;
+    private static final int EXEMPTION_FROM_DISCOUNTS = 900;
+    private static final int SALARY_WITH_FIVE_PERCENT_DISCOUNT = 1500;
+    private static final int SALARY_WITH_TEN_PERCENT_DISCOUNT = 2500;
+    private static final double FIVE_PERCENT_DISCOUNT = 0.05;
+    private static final double TEN_PERCENT_DISCOUNT = 0.1;
+    private static final double TWENTY_PERCENT_DISCOUNT = 0.2;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double grossSalary;
-        double netSalary;
         double salaryDiscountIR;
-        double salaryDiscountSyndicate;
-        double salaryDiscountFGTS;
-        double totalDiscounts;
-        int workedHours;
-        int workedDays;
 
         System.out.println("Digite quanto você ganha por hora:");
-        workedHours = input.nextInt();
+        int workedHours = input.nextInt();
 
         System.out.println("Digite quantos dias você trabalhou:");
-        workedDays = input.nextInt();
+        int workedDays = input.nextInt();
 
-        grossSalary = workedHours * workedDays;
-        salaryDiscountSyndicate = grossSalary * THREE_PERCENT_DISCOUNT;
-        salaryDiscountFGTS = grossSalary * ELEVEN_PERCENT_FGTS;
+        double grossSalary = workedHours * workedDays;
+        double salaryDiscountSyndicate = grossSalary * THREE_PERCENT_DISCOUNT;
+        double salaryDiscountFGTS = grossSalary * ELEVEN_PERCENT_FGTS;
 
         if (grossSalary <= EXEMPTION_FROM_DISCOUNTS) {
             salaryDiscountIR = ZERO;
@@ -45,8 +38,8 @@ public class exercice_12 {
             salaryDiscountIR = grossSalary * TWENTY_PERCENT_DISCOUNT;
         }
 
-        totalDiscounts = salaryDiscountIR + salaryDiscountSyndicate;
-        netSalary = grossSalary - salaryDiscountIR - salaryDiscountSyndicate;
+        double totalDiscounts = salaryDiscountIR + salaryDiscountSyndicate;
+        double netSalary = grossSalary - salaryDiscountIR - salaryDiscountSyndicate;
 
         System.out.println("Salário Bruto: " + grossSalary +
                 "\nIR: " + salaryDiscountIR +
