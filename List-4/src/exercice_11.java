ca45b89d55ee8fdabc6697a0b830c5b2706d31e5
RafaelKeiti <keitiazuma@gmail.com>
2023-04-06 23:28:20 -0300

    feat: Programa que cria uma matriz e determina o maior número e sua posição de linha e coluna.

---
+++ List-4/src/exercice_11.java
@@ -0,0 +1,36 @@
+public class exercice_11 {
+
+    private static final int ZERO_START = 0;
+    private static final int TIMES_TEN = 10;
+
+    public static void main(String[] args) {
+        int[][] array = new int[4][4];
+        int higherNumber = 0;
+        int positionLine = 0;
+        int positionColumn = 0;
+
+        for (int i = ZERO_START; i < array.length; i++) {
+            for (int j = ZERO_START; j < array[i].length; j++) {
+                array[i][j] = (int) (Math.random() * TIMES_TEN);
+            }
+        }
+
+        for (int i = ZERO_START; i < array.length; i++) {
+            for (int j = ZERO_START; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " | ");
+            }
+            System.out.println();
+        }
+
+        for (int i = ZERO_START; i < array.length; i++) {
+            for (int j = ZERO_START; j < array[i].length; j++) {
+                if (array[i][j] > higherNumber) {
+                    higherNumber = array[i][j];
+                    positionLine = i;
+                    positionColumn = j;
+                }
+            }
+        }
+        System.out.println("Maior número: " + higherNumber + "\nPosição linha: " + positionLine + "\nPosição coluna: " + positionColumn);
+    }
+}
