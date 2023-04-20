60fb2dd2f595dba915b5bfea71446a6f4042ef5b
RafaelKeiti <keitiazuma@gmail.com>
2023-04-20 00:54:02 -0300

    refactor: Retirando sinal  de (+) utilizado de forma desnecessária.

--- List-2/src/exercice_09.java
+++ List-2/src/exercice_09.java
@@ -15,21 +15,21 @@ public class exercice_09 {
 
         if (firstNumber > secondNumber && firstNumber > thirdNumber) {
             if (secondNumber > thirdNumber) {
-                System.out.println(+firstNumber + " / " + secondNumber + " / " + thirdNumber);
+                System.out.println(firstNumber + " / " + secondNumber + " / " + thirdNumber);
             } else {
-                System.out.println(+firstNumber + " / " + thirdNumber + " / " + secondNumber);
+                System.out.println(firstNumber + " / " + thirdNumber + " / " + secondNumber);
             }
         } else if (secondNumber > firstNumber && secondNumber > thirdNumber) {
             if (firstNumber > thirdNumber) {
-                System.out.println(+secondNumber + " / " + firstNumber + " / " + thirdNumber);
+                System.out.println(secondNumber + " / " + firstNumber + " / " + thirdNumber);
             } else {
-                System.out.println(+secondNumber + " / " + thirdNumber + " / " + firstNumber);
+                System.out.println(secondNumber + " / " + thirdNumber + " / " + firstNumber);
             }
         } else if (thirdNumber > firstNumber && thirdNumber > secondNumber) {
             if (firstNumber > secondNumber) {
-                System.out.println(+thirdNumber + " / " + firstNumber + " / " + secondNumber);
+                System.out.println(thirdNumber + " / " + firstNumber + " / " + secondNumber);
             } else {
-                System.out.println(+thirdNumber + " / " + secondNumber + " / " + firstNumber);
+                System.out.println(thirdNumber + " / " + secondNumber + " / " + firstNumber);
             }
         } else {
             System.out.println("Digite apenas números diferentes um do outro.");
