84eb5c1cfb191f41e9e067035d6afa7a3a862c68
RafaelKeiti <keitiazuma@gmail.com>
2023-04-29 21:25:40 -0300

    refactor: Alterando forma de receber números no vectorA.

--- List-4/src/exercice_03.java
+++ List-4/src/exercice_03.java
@@ -3,8 +3,12 @@ public class exercice_03 {
     private static final int ZERO_START = 0;
 
     public static void main(String[] args) {
-        int[] vectorA = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        int[] vectorA = new int[15];
         int[] vectorB = new int[15];
+
+        for (int i = ZERO_START; i < vectorA.length; i++) {
+            vectorA[i] = i;
+        }
 
         for (int i = ZERO_START; i < vectorB.length; i++) {
             vectorB[i] = vectorA[i] * vectorA[i];
