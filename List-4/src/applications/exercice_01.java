998382089b8d32e433ca08b651dc5cf0254d27f1
RafaelKeiti <keitiazuma@gmail.com>
2023-04-30 21:06:44 -0300

    refactor: Adicionando (final) nas variáveis.

--- List-4/src/applications/exercice_01.java
+++ List-4/src/applications/exercice_01.java
@@ -5,8 +5,8 @@ import static utils.VectorUtils.*;
 public class exercice_01 {
 
     public static void main(String[] args) {
-        int[] vectorA = new int[]{1, 2, 3, 4, 5};
-        int[] vectorB = new int[5];
+        final int[] vectorA = new int[]{1, 2, 3, 4, 5};
+        final int[] vectorB = new int[5];
 
         for (int i = ZERO_START; i < vectorB.length; i++) {
             vectorB[i] = vectorA[i];
