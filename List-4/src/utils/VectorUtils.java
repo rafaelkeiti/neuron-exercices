51dfe4fa0cce769d55648e39fefa14d5f36c2861
RafaelKeiti <keitiazuma@gmail.com>
2023-04-30 21:06:07 -0300

    refactor: Adicionando (final) nas variáveis.

--- List-4/src/utils/VectorUtils.java
+++ List-4/src/utils/VectorUtils.java
@@ -4,21 +4,21 @@ public class VectorUtils {
 
     public static final int ZERO_START = 0;
 
-    public static void printVector(String s, int[] vectorA) {
+    public static void printVector(final String s, final int[] vectorA) {
         System.out.print("\n" + s + " - ");
         for (int j : vectorA) {
             System.out.print(j + " | ");
         }
     }
 
-    public static void printVector(String s, double[] vectorB) {
+    public static void printVector(final String s, final double[] vectorB) {
         System.out.print("\n" + s + " - ");
         for (double v : vectorB) {
             System.out.printf("%.2f | ", v);
         }
     }
 
-    public static void printArray(int[][] array) {
+    public static void printArray(final int[][] array) {
         for (int i = ZERO_START; i < array.length; i++) {
             for (int j = ZERO_START; j < array[i].length; j++) {
                 System.out.print(array[i][j] + " | ");
@@ -27,7 +27,7 @@ public class VectorUtils {
         }
     }
 
-    public static void generateValuesVector(int[] vectorA) {
+    public static void generateValuesVector(final int[] vectorA) {
         for (int i = ZERO_START; i < vectorA.length; i++) {
             vectorA[i] = i;
         }
